51c74e3afbc15e46e96a814126ae7675fe85a454
Gerry Gonzaga <gdgonzaga@gmail.com>
2013-02-08 17:50:55 +0800

    Added comments

--- bin/dmpd-select.sh
+++ bin/dmpd-select.sh
@@ -1,63 +1,121 @@
 #!/bin/bash
+#
+# This script was meant to replicate the the mpd module from stumpwm,
+# but in a WM agnostic way. It's meant to be used with xchainkeys.
+#
+# Depends on: mpc, dmenu and compton
+#
+# Exit codes:
+#       1 Incorrect usage
+#       2 add: Track already in playlist
+#
 # TODO: delete playlist
 # TODO: catch errors
 
-FONT="-*-dina-medium-r-normal-*-*-*-*-*-*-*-*-*"
-N_LIST_ITEMS=20
-PLAYLIST_DIR="$HOME/.mpd/playlists"
-DMENU_QUERY="dmenu -fn $FONT -i -l $N_LIST_ITEMS"
+function show_usage {
+    echo  "Usage: dmpd-select.sh <command>
+Commands:
+       song       Select track to play
+       artist     Add artist to current playlist
+       cartist    Clear current playlist first
+       album      Add album to current playlist
+       calbum     Clear current playlist first
+       playlist   Add the contents of playlist to current playlist
+       cplaylist  Clear current playlist first
+       save       Saves the current playlist
+       add        Add current song to playlist
+       all        Clear current playlist and add all songs"
 
-function transparent {
-    sleep 0.1; compton-trans -w $(xwininfo -root -children | grep 1600x | head -1 | cut -d' ' -f6) 90
+    exit 1
 }
 
+PLAYLIST_DIR="$HOME/.mpd/playlists"
+
+# dmenu-specific
+FONT="-*-dina-medium-r-normal-*-*-*-*-*-*-*-*-*"
+N_LIST_ITEMS=20
+DMENU_QUERY="dmenu -fn $FONT -i -l $N_LIST_ITEMS"
+
+# Transparency options
+SLEEP_DELAY=0.1
+OPACITY=90
+
+
+# Make dmenu transparent. Run just before invoking dmenu.
+# Usage:   transparent
+function transparent {
+    (sleep $SLEEP_DELAY; compton-trans -w $(xwininfo -root -children | grep 1600x | head -1 | cut -d' ' -f6) $OPACITY) &
+}
+
+# Use dmenu to list all songs in the current playlist and play whichever
+# is chosen.
 function select_song {
     MPC_FORMAT="%artist% (%album%) - [%title%]|[%file%]"
-    transparent &
+    transparent
     SONG_NUMBER=$(mpc playlist -f "$MPC_FORMAT" |
                   nl -s ")  " |
                   $DMENU_QUERY -p "Search song" |
                   cut -d")" -f1)
-
     [ -n "$SONG_NUMBER" ] && mpc play $SONG_NUMBER
 }
 
+# Returns the item chosen from dmenu.
+# Usage:   query tag [prompt]
+# Example: query artist "Enter artist name"
 function query {
     TAG="$1"
     [ -n "$2" ] && PROMPT="$2"
-    transparent &
+    transparent
     echo -n $(mpc list "$TAG" | sort | $DMENU_QUERY -p "$PROMPT")
 }
 
+# Add songs that match the tag-query pair to the current playlist
+# Usage:   add tag string
+# Example: add artist "Pearl Jam"
 function add {
     TYPE=$1
     QUERY="$2"
     mpc search $TYPE "$QUERY" | mpc add
 }
 
+# Ask the user for [tag] and add all matching tracks to the current
+# playlist.
+# Usage:   query_add tag
+# Example: query_add artist
 function query_add {
    add $1 "$(query $1 "Add $1")"
 }
 
+# Like query_add, but clears the current playlist first.
+# Usage:   clear_query_add tag
+# Example: clear_query_add artist
 function clear_query_add {
     TERM=$(query "$1" "Load $1")
     [ -n "$TERM" ] && mpc clear && add $1 "$TERM"
 }
 
+# Add the contents of [playlist] to the current playlist.
+# Usage:   load_playlist
 function load_playlist {
     mpc lsplaylists | $DMENU_QUERY -p "Load playlist" | mpc load
 }
 
+# Like load_playlist, but clear the current playlist first.
+# Usage:   clear_load_playlist
 function clear_load_playlist {
     PLAYLIST="$(mpc lsplaylists | $DMENU_QUERY -p "Load playlist")"
     [ -n "$PLAYLIST" ] && mpc clear && mpc load "$PLAYLIST"
 }
 
+# Save the current [playlist]. Uses dmenu to get the [playlist].
+# Usage:   save_playlist
 function save_playlist {
     FILE="$(mpc lsplaylists | $DMENU_QUERY -p "Save playlist")"
     [ -n "$FILE" ] && mpc save "$FILE"
 }
 
+# Add the current song to [playlist]. Uses dmenu to get the [playlist].
+# Usage:   current_to_playlist
 function current_to_playlist {
     PLAYLIST_NAME="$(ls "$PLAYLIST_DIR" | sed 's/\.m3u$//' |
                    $DMENU_QUERY -p "Add current song to playlist")"
@@ -92,4 +150,6 @@ case $1 in
     add) current_to_playlist
         ;;
     all) mpc clear; mpc listall | mpc add && echo "[all songs loaded]"
+        ;;
+    *) show_usage
 esac
