36c586db3dbe79f3987017c5cba26c51bc3dc0f9
Gerry Gonzaga <gdgonzaga@gmail.com>
2013-02-08 09:30:45 +0800

    Added dmenu prompts

--- bin/dmpd-select.sh
+++ bin/dmpd-select.sh
@@ -4,22 +4,23 @@
 
 FONT="-*-dina-medium-r-normal-*-*-*-*-*-*-*-*-*"
 N_LIST_ITEMS=20
-DMENU_QUERY="dmenu -fn $FONT -i -l $N_LIST_ITEMS"
 PLAYLIST_DIR="$HOME/.mpd/playlists"
-
+DMENU_QUERY="dmenu -fn $FONT -i -l $N_LIST_ITEMS"
 
 function select_song {
     MPC_FORMAT="%artist% (%album%) - [%title%]|[%file%]"
     SONG_NUMBER=$(mpc playlist -f "$MPC_FORMAT" |
                   nl -s ")  " |
-                  $DMENU_QUERY |
+                  $DMENU_QUERY -p "Search song" |
                   cut -d")" -f1)
 
     [ -n "$SONG_NUMBER" ] && mpc play $SONG_NUMBER
 }
 
 function query {
-    echo -n $(mpc list "$1" | sort | $DMENU_QUERY)
+    TAG="$1"
+    [ -n "$2" ] && PROMPT="$2"
+    echo -n $(mpc list "$TAG" | sort | $DMENU_QUERY -p "$PROMPT")
 }
 
 function add {
@@ -29,30 +30,31 @@ function add {
 }
 
 function query_add {
-   add $1 "$(query $1)"
+   add $1 "$(query $1 "Add $1")"
 }
 
 function clear_query_add {
-    TERM=$(query "$1")
+    TERM=$(query "$1" "Load $1")
     [ -n "$TERM" ] && mpc clear && add $1 "$TERM"
 }
 
 function load_playlist {
-    mpc lsplaylists | $DMENU_QUERY | mpc load
+    mpc lsplaylists | $DMENU_QUERY -p "Load playlist" | mpc load
 }
 
 function clear_load_playlist {
-    PLAYLIST="$(mpc lsplaylists | $DMENU_QUERY)"
+    PLAYLIST="$(mpc lsplaylists | $DMENU_QUERY -p "Load playlist")"
     [ -n "$PLAYLIST" ] && mpc clear && mpc load "$PLAYLIST"
 }
 
 function save_playlist {
-    FILE="$(mpc lsplaylists | $DMENU_QUERY)"
+    FILE="$(mpc lsplaylists | $DMENU_QUERY -p "Save playlist")"
     [ -n "$FILE" ] && mpc save "$FILE"
 }
 
 function current_to_playlist {
-    PLAYLIST_NAME="$(ls "$PLAYLIST_DIR" | sed 's/\.m3u$//' | $DMENU_QUERY)"
+    PLAYLIST_NAME="$(ls "$PLAYLIST_DIR" | sed 's/\.m3u$//' |
+                   $DMENU_QUERY -p "Add current song to playlist")"
     PLAYLIST_FILE="$PLAYLIST_DIR/$PLAYLIST_NAME.m3u"
 
     if [ -n "$(cat "$PLAYLIST_FILE" | grep "$(mpc -f %file% current)")" ]; then
@@ -67,11 +69,11 @@ function current_to_playlist {
 case $1 in
     song) select_song
         ;;
-    artist) artist
+    artist) query_add artist
         ;;
     cartist) clear_query_add artist
         ;;
-    album) album
+    album) query_add album
         ;;
     calbum) clear_query_add album
         ;;
