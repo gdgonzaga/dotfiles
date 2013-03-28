86a4b2141a558d4d0a2fff09393a94009a63576f
Gerry Gonzaga <gdgonzaga@gmail.com>
2013-03-28 10:47:13 +0800

    Use a whiter shade of white

--- dwm/config.h
+++ dwm/config.h
@@ -4,11 +4,12 @@
 static const char font[]            = "-*-dina-medium-r-*-*-*-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#222222";
 static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
+static const char normfgcolor[]     = "#ffffff";
+//static const char normfgcolor[]     = "#bbbbbb";
 static const char selbordercolor[]  = "#005577";
 static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
